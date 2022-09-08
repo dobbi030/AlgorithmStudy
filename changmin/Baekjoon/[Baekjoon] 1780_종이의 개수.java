ed10027c01370bcdfa3ec5dc4b0bf57144075fa8
Lee-Chang Min <43628924+cmlee0913@users.noreply.github.com>
2022-09-08 20:59:14 +0900

    주석 달기

--- changmin/Baekjoon/[Baekjoon] 1780_종이의 개수.java
+++ changmin/Baekjoon/[Baekjoon] 1780_종이의 개수.java
@@ -6,19 +6,19 @@ public class BJ1780 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static int boardSize;
     static int[][] board;
-    static int[] numSize = { 0, 0, 0 };
+    static int[] numSize = { 0, 0, 0 }; // -1, 0, 1의 개수
 
     static void recursion(int size, int startY, int startX) {
-        int pivot = board[startY][startX];
+        int pivot = board[startY][startX]; // 첫 번째 종이를 기준으로 한다.
 
-        if (size == 1) {
+        if (size == 1) { // 종이의 사이즈가 1 이면 그냥 값을 올려준다.
             numSize[pivot + 1]++;
             return;
         }
 
         for (int i = startY; i < startY + size; ++i) {
             for (int j = startX; j < startX + size; ++j) {
-                if (board[i][j] != pivot) {
+                if (board[i][j] != pivot) { // 종이의 숫자가 다르면 9개로 나누고 현재 함수를 닫는다.
                     recursion(size / 3, startY, startX);
                     recursion(size / 3, startY, startX + (size / 3));
                     recursion(size / 3, startY, startX + (size * 2 / 3));
@@ -33,7 +33,7 @@ public class BJ1780 {
             }
         }
 
-        numSize[pivot + 1]++;
+        numSize[pivot + 1]++; // 모든 종이가 같으면 해당하는 값을 올려준다.
     }
 
     public static void main(String[] args) throws NumberFormatException, IOException {
@@ -47,7 +47,7 @@ public class BJ1780 {
             }
         }
 
-        recursion(boardSize, 0, 0);
-        System.out.printf("%d\n%d\n%d", numSize[0], numSize[1], numSize[2]);
+        recursion(boardSize, 0, 0); // 재귀 시작
+        System.out.printf("%d\n%d\n%d", numSize[0], numSize[1], numSize[2]); // 결과값을 출력한다.
     }
 }
